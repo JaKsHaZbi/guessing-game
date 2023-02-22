23eadc1e07f87a761352e77cb0c999df43b1e17c
JaKsHaZbi <Joker_hazbi@mail.ru>
2023-02-22 09:50:39 +0300

    feat: fill GuessingGame class. Finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.start = min
+        this.end = max
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.start + this.end) / 2)
+        return this.middle
     }
 
     lower() {
-
+        return this.end = this.middle
     }
 
     greater() {
-
+        return this.start = this.middle
     }
 }
 
